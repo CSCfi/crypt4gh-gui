364f061f5af6440afe6cf8d5a3621f51833a19cf
teemu.kataja <teemu.kataja@csc.fi>
2020-10-20 15:20:42 +0300

    make script installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="sds_uploader",
+    version="0.1.0",
+    license="Apache-2.0",
+    author="CSC - IT Center for Science Ltd.",
+    author_email="",
+    description="FUSE client for accessing the SDA.",
+    long_description="",
+    packages=["crypt4gh_gui"],
+    entry_points={"console_scripts": ["sds_uploader=crypt4gh_gui.crypt4gh_gui:main"]},
+    platforms="any",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
+    ],
+    install_requires=["crypt4gh", "paramiko"],
+)
